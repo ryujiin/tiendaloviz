fb4f4bc5680a43d027e399a55d651a21ce1a3a40
ryujiin <ryujiin22@gmail.com>
2015-01-01 10:18:16 -0500

    lista pa gunicorn

--- apiloviz/wsgi.py
+++ apiloviz/wsgi.py
@@ -12,10 +12,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apiloviz.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-try:
-	from dj_static import Cling
-
-	application = Cling(get_wsgi_application())
-except:
-	pass
